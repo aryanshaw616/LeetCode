7949dd7be6819114e644ebd681508e83515a58da
Aryan Kumar Shaw Halwai <135535751+aryanshaw616@users.noreply.github.com>
2025-02-07 19:29:27 +0530

    Time: 1 ms (96.49%), Space: 45.4 MB (64.51%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int a[]=new int[nums.length*2];
+        for(int i=0;i<a.length;i++)
+            a[i]=nums[i%nums.length];
+            return a;
+    }
+}
