e45a14a07e966c3307c5b22cbac0eca080970d1a
Aryan Kumar Shaw Halwai <135535751+aryanshaw616@users.noreply.github.com>
2025-03-25 15:36:04 +0530

    Time: 1 ms (86.06%), Space: 40.5 MB (98.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,27 @@
 class Solution {
     public int mySqrt(int x) {
-        int k = (int) Math.sqrt(x);
-        return k;
+        if(x==0 || x==1) return x;
+        else
+        return bin(x);
+    }
+    static int bin(int n)
+    {
+        int l=0,r=n-1,m=0,res=0;
+        while(l<=r)
+        {
+            m=l+(r-l)/2;
+            long m2= (long)m*m;
+            if(m2==n)
+            return m;
+            if(m2<n)
+            {
+                res=m;
+                l=m+1;
+            }
+            else
+            r=m-1;
+            
+        }
+        return res;
     }
 }
