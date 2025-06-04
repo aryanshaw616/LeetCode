c08b84f981aa776be869c8c6be03929064f28f1a
Aryan Kumar Shaw Halwai <135535751+aryanshaw616@users.noreply.github.com>
2025-06-04 07:06:21 +0530

    Time: 4 ms (87.96%), Space: 45.4 MB (68.52%) - LeetHub

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void func(int i, int j, String word, String[] str) {
+        String s = word.substring(i, j);
+        if (str[0].equals("")) {
+            str[0] = s;
+            return;
+        }
+        if (str[0].compareTo(s) < 0) {
+            str[0] = s;
+        }
+    }
+
+    public String answerString(String word, int numFriends) {
+        if(numFriends == 1) return word;
+        int n = word.length();
+        int k = n - numFriends + 1;
+        char ch = 'a';
+
+        for (int i = 0; i < n; i++) {
+            if (word.charAt(i) > ch) {
+                ch = word.charAt(i);
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (word.charAt(i) == ch) {
+                list.add(i);
+            }
+        }
+
+        String[] str = new String[]{""}; // ✅ Fix: Initialize with empty string
+
+        for (int i = 0; i < n; i++) {
+            if (word.charAt(i) != ch) continue;
+            int j = Math.min(n, i + k);
+            func(i, j, word, str);
+        }
+
+        return str[0];
+    }
+}
